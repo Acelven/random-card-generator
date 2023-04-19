795a9cb13a5b557317d704913391465312302583
Acelven <acelven@hotmail.com>
2023-04-19 02:16:05 +0000

    RCG 1.0

--- src/app.js
+++ src/app.js
@@ -1,11 +1,58 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let suits, numbers, suitsBottom, i;
+var card = {
+  suits: ["&#9830", "&#9829", "&#9824", "&#9827"],
+  suitsBottom: ["&#9830", "&#9829", "&#9824", "&#9827"],
+  numbers: ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+};
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function pullNumber() {
+  var theNumber = Math.floor(Math.random() * card.numbers.length);
+  document.getElementById("randomNumber").innerHTML = card.numbers[theNumber];
+}
+
+function pullBothSuits() {
+  var allSuits = Math.floor(Math.random() * card.suits.length);
+  document.getElementById("topSuit").innerHTML = card.suits[allSuits];
+  document.getElementById("bottomSuit").innerHTML = card.suits[allSuits];
+
+  if (card.suits[allSuits] === "&#9830" || card.suits[allSuits] === "&#9829") {
+    document.getElementById("aCard").classList.add("redSuit");
+  } else if (
+    card.suits[allSuits] === "&#9827" ||
+    card.suits[allSuits] === "&#9824"
+  ) {
+    document.getElementById("aCard").classList.remove("redSuit");
+  }
+}
+
+function startTimer(duration, display) {
+  var timer = duration,
+    // minutes,
+    seconds;
+  setInterval(function() {
+    // minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    // minutes = minutes < 10 ? "0" + minutes : minutes;
+    // seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = seconds;
+
+    if (--timer < 0) {
+      pullNumber();
+      pullBothSuits();
+      timer = duration;
+    }
+  }, 1000);
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var fiveSeconds = 5,
+    display = document.querySelector("#time");
+  startTimer(fiveSeconds, display);
+};
+
+window.wholeCard = function() {
+  pullNumber();
+  pullBothSuits();
 };
